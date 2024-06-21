5a6e88c378ab910470c7e7294d9ff2c066293476
Infinitech <157631886+Inifinitech@users.noreply.github.com>
2024-06-21 18:05:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,46 +2,46 @@
 function add(a, b) {
     return a + b;
 } 
-console.log(add(9,4));
+console.log(add());
 
 // function to subtract
 function subtract(a, b) {
     return a - b;
 } 
-console.log(subtract(9,6));
+console.log(subtract());
 
 // function to multiply
 function multiply(a, b) {
     return a * b;
 } 
-console.log(multiply(9,7));
+console.log(multiply());
 
 // function to divide
 function divide(a, b) {
     return a / b;
 } 
-console.log(divide(9, 9));
+console.log(divide());
 
 // function to increment
 function increment(n) {
     return ++n;
 }
-console.log(increment(9));
+console.log(increment());
 
 // function to decrement
 function decrement(n) {
     return --n;
 }
-console.log(decrement(9));
+console.log(decrement());
 
 // function to parse a string
 function makeInt(string) {
     return parseInt(string, 10);
 }
-console.log(makeInt("70.9999991"));
+console.log(makeInt());
 
 // function to parse a string and return it as a float
 function preserveDecimal(string) {
     return parseFloat(string);
 }
-console.log(preserveDecimal("993.93900"));
+console.log(preserveDecimal());
